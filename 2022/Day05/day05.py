8d95923be1cf2f4d821d58e425ea94064b26bf06
Tobias Spitaler <spitaler.t@DMWZONE.local>
2024-02-02 14:33:56 +0100

    Removing input files from 2022

--- 2022/Day05/day05.py
+++ 2022/Day05/day05.py
@@ -8,39 +8,27 @@ Created on Fri Nov 25 15:59:05 2022
 import numpy as np
 import copy
 
-
-"""
-        [H]     [W] [B]            
-    [D] [B]     [L] [G] [N]        
-[P] [J] [T]     [M] [R] [D]        
-[V] [F] [V]     [F] [Z] [B]     [C]
-[Z] [V] [S]     [G] [H] [C] [Q] [R]
-[W] [W] [L] [J] [B] [V] [P] [B] [Z]
-[D] [S] [M] [S] [Z] [W] [J] [T] [G]
-[T] [L] [Z] [R] [C] [Q] [V] [P] [H]
- 1   2   3   4   5   6   7   8   9 """
+with open("input.txt") as f:
+    A = f.read()
+    stacks_str, A_input = A.split("\n\n")
+    
 
 stacks = {}
+for i in range(1,10):
+    stacks[i] = []
 
-stacks[1] = ['T','D','W','Z','V','P']
-stacks[2] = ['L','S','W','V','F','J','D']
-stacks[3] = ['Z','M','L','S','V','T','B','H']
-stacks[4] = ['R','S','J']
-stacks[5] = ['C','Z','B','G','F','M','L','W']
-stacks[6] = ['Q','W','V','H','Z','R','G','B']
-stacks[7] = ['V','J','P','C','B','D','N']
-stacks[8] = ['P','T','B','Q']
-stacks[9] = ['H','G','Z','R','C']
+
+indices_of_letters = list(range(1,1+9*4,4))
+for stack_line in stacks_str.split("\n")[:-1]:
+    for i, let_pos in enumerate(indices_of_letters):
+        if stack_line[let_pos] != " ":
+            stacks[i+1].insert(0, stack_line[let_pos])
+        
 
 
 stacks2 = copy.deepcopy(stacks)
 
-
-with open('input.txt', 'r') as f:
-    A_input = f.read().split('\n')
-
-
-for line in A_input:
+for line in A_input.split("\n"):
     words = line.split()
     
     for i in range(int(words[1])):
@@ -60,7 +48,7 @@ print(word)
 
 ## %%part two
 
-for line in A_input:
+for line in A_input.split("\n"):
     words = line.split()
     
     amount = int(words[1]) 
